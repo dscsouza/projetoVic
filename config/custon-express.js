4785bd58929864a5fc5fd2b4e41ec2f87f51a0f5
Victor Alexandre <alexanvictor@gmail.com>
2023-03-10 20:06:41 -0400

    Ajuste da view engine

--- config/custon-express.js
+++ config/custon-express.js
@@ -1,10 +1,16 @@
 const express = require('express'); //importando o express
 const consign = require('consign'); //importando o consign
+const path = require('path');
 
 module.exports = () => {
     const app = express(); //Criando o app
+    app.use('/', express.static(path.join(__dirname, "../static")));
     app.use(express.json()); 
     app.use(express.urlencoded({extended: true}));
+    app.set('view engine', 'ejs');
+    app.set('views', './views');
+    
+    
     //Incluindo a pasta de rotas no app
     consign()
         .include('routes')
