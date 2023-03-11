3e1947aefb20fa7dcf347cc5fcb08e169814bc93
dscsouza <dscsouza@gmail.com>
2023-03-10 21:36:39 -0400

    alteração na query, para retornar user e hash baseado no user

--- model/userModel.js
+++ model/userModel.js
@@ -1,16 +1,26 @@
 const connection = require('../database/connection');
 
 class User {
-    autentica(username, hash){
-        const sql = `SELECT nm_usuario, hash_senha FROM tb_usuario WHERE nm_usuario = ${username} AND hash_senha = ${hash}`;
+    autentica(username){
+        const sql = `SELECT nm_usuario, hash_senha FROM tb_usuario WHERE nm_usuario = ${username}`;
         connection.query(sql, (error, result) => {
             if(error){
-                return error;
+                console.log('if aqui')
+                console.log(error)
+                return false;
             }else{
-                return JSON.parse(result);
+                console.log('else aqui')
+                return result;
             }
         })
     }
+
+    encrypt(password){
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(password, salt);
+        return hash;
+    }
+
 }
 
 module.exports = new User;
