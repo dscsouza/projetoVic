3f5f3234e99a093db461dc0466e9cd18ae7d0794
DeyviMacOS <dscsouza@gmail.com>
2023-03-11 11:36:45 -0400

    ajuste na rota /protegido. autenticar pelo header

--- routes/login.js
+++ routes/login.js
@@ -5,8 +5,9 @@ module.exports = app => {
     app.post('/login', userController.login);
 
     // Rota protegida por autenticação
-    app.get('/protegido/:acessToken', authenticateToken, (req, res) => {
-        res.render('dashboard', {user: req.user})
+    app.get('/protegido', authenticateToken, (req, res) => {
+      console.log('autenticado...')  
+      res.render('dashboard', {user: req.user.username})
       });
     
     //para fins de testes essa rota encontra-se desprotegida
