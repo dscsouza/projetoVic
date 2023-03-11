98008204ed919f7d24d8e1bf9af6e5e7f61ba009
DeyviMacOS <dscsouza@gmail.com>
2023-03-11 01:39:59 -0400

    incluida rota de abertura de os - /inclui_os

--- routes/login.js
+++ routes/login.js
@@ -8,6 +8,15 @@ module.exports = app => {
     app.get('/protegido/:acessToken', authenticateToken, (req, res) => {
         res.render('dashboard', {'user': req.user.username})
       });
+    
+    //para fins de testes essa rota encontra-se desprotegida
+    //antes de ir para produção, reincluir o middleware authenticateToken
+    app.get('/inclui_os', (req, res) => {
+      res.render('abertura_os')
+    });
+
+
+
 }
 
 
