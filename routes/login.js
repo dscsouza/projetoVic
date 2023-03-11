8fe05def6f7fba0b898eea7ed98e724f7ea2fee3
Victor Alexandre <alexanvictor@gmail.com>
2023-03-10 20:08:19 -0400

    Unificando as rotas de login no arquivo login.js

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,17 @@
-var express = require('express');
-var router = express.Router();
-const bcrypt = require('bcryptjs');
 const userController = require('../controllers/userControllers');
+const authenticateToken = require('../middlewares/authenticateToken');
+
+module.exports = app => {
+    app.post('/login', userController.login);
+
+    // Rota protegida por autenticação
+    app.get('/protegido/:acessToken', authenticateToken, (req, res) => {
+        res.render('dashboard', {'user': req.user.username})
+      });
+}
 
 
+  
 
-// Rota de login
-router.post('/', userController.login);
 
-module.exports = router;
   
