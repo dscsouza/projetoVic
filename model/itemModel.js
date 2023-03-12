a465b7ffe18a84e5c1d7cffb15f4e297d6745158
Victor Alexandre <alexanvictor@gmail.com>
2023-03-11 23:09:52 -0400

    Realizado o ajuste na desconstrução da variavel dados e nos parametros da procedure para cadastro do item

--- model/itemModel.js
+++ model/itemModel.js
@@ -3,8 +3,8 @@ const connection = require('../database/connection');
 class Item {
     create(dados){
     return new Promise((resolve, reject) => {
-        const [ds_item, referencia, tipo] = dados
-        const sql = `CALL prd_cadastra_item(${ds_item}, ${referencia}, ${tipo})`;
+        const {ds_item, referencia, tipo} = dados
+        const sql = `CALL prd_cadastra_item(?, ?, ?)`;
         connection.query(sql, [ds_item, referencia, tipo], (error, results) => {
             if(error){
                 reject(error);
