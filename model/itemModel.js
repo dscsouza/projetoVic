4eb53f17f9ba45ab23dc23cf8bbb2c5bd589be60
Victor Alexandre <alexanvictor@gmail.com>
2023-03-11 22:40:51 -0400

    Criada a model com o método create para registrar os dados dos itens no banco

---
+++ model/itemModel.js
@@ -0,0 +1,17 @@
+const connection = require('../database/connection');
+
+class Item {
+    create(dados){
+    return new Promise((resolve, reject) => {
+        const [ds_item, referencia, tipo] = dados
+        const sql = `CALL prd_cadastra_item(${ds_item}, ${referencia}, ${tipo})`;
+        connection.query(sql, [ds_item, referencia, tipo], (error, results) => {
+            if(error){
+                reject(error);
+            }else{
+                resolve(results);
+            }
+        })
+    })
+  }
+}
