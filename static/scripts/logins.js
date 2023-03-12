155a0ba8a6db1651342be090fb123a7dcf03b90a
DeyviMacOS <dscsouza@gmail.com>
2023-03-12 02:20:05 -0400

    injetando a resposta html na div ao inves de usar document.write(data)

--- static/scripts/logins.js
+++ static/scripts/logins.js
@@ -18,7 +18,8 @@ function request_autenticado(rota){
     return response.text();
   })
   .then(data => {
-    document.write(data);
+    principal = document.getElementById('htmlPrincipal');
+    principal.innerHTML = data
   })
   .catch(error => {
     console.error(error);
@@ -56,3 +57,27 @@ form.addEventListener('submit', event => {
     topContainer.classList.toggle("d-none")
     })
 })
+
+function abreOs(){
+  history.pushState({}, '', '/abreOs');
+  request_autenticado('/abreOs')
+
+}
+
+function cadastraClientes(){
+  history.pushState({}, '', '/cadastraCliente');
+  request_autenticado('/cadastraCliente')
+
+}
+
+function incluItem(){
+  history.pushState({}, '', '/estoque');
+  request_autenticado('/estoque')
+
+}
+
+function incluiUsuario(){
+  history.pushState({}, '', '/inclui_usuario');
+  request_autenticado('/inclui_usuario')
+
+}
