18d15aa20594667eb45fca663cf1e8901fd17262
DeyviMacOS <dscsouza@gmail.com>
2023-03-12 02:14:30 -0400

    javascript da tela de cadastro de cliente - em andamento

---
+++ static/scripts/cadastraCliente.js
@@ -0,0 +1,59 @@
+const form = document.getElementById('cadastroClientes');
+const message = document.getElementById('message');
+const topContainer = document.getElementById('alertTopContainer')
+console.log('arquivo js do html')
+
+function request_autenticado(rota){
+  const accessToken = localStorage.getItem('accessToken');
+
+  fetch(rota, {
+    headers: {
+      'Authorization': `${accessToken}`
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Não foi possível acessar a rota ${rota}`);
+    }
+    return response.text();
+  })
+  .then(data => {
+    principal = document.getElementById('htmlPrincipal');
+    principal.innerHTML = data
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const username = form.elements.username.value;
+  const password = form.elements.password.value;
+  const data = { username, password };
+
+  fetch('/login', {
+	method: 'POST',
+	headers: {
+  	'Content-Type': 'application/json'
+	},
+	body: JSON.stringify(data)
+  })
+  .then(response => {
+	if (!response.ok) {
+  	throw new Error(response.statusText);
+	}
+	return response.json();
+  })
+  .then(data => {
+	localStorage.setItem('accessToken', data.accessToken);
+  request_autenticado('/protegido')
+
+  })
+  .catch(error => {
+	console.error(error);
+	message.innerText = 'Usuário ou senha incorretos.';
+    topContainer.classList.toggle("d-none")
+    })
+})
