1e5bba15cc40d416e0ac7540669cbee89b14bc49
Victor Alexandre <alexanvictor@gmail.com>
2023-03-12 21:29:00 -0400

    Adicionado os campos de EMAIL e TELEFONE

--- model/clienteModel.js
+++ model/clienteModel.js
@@ -2,11 +2,11 @@ const connection = require('../database/connection');
 
 class Cliente {
         create(dados){
-        const {nm_cliente, cgc, contato} = dados
-        const sql = 'CALL prd_cadastra_cliente(?, ?, ?)'
+        const {nm_cliente, cgc, email, telefone} = dados
+        const sql = 'CALL prd_cadastra_cliente(?, ?, ?, ?)'
         
         return new Promise((resolve, reject) => {
-            connection.query(sql, [nm_cliente, cgc, contato], (error, results) =>{
+            connection.query(sql, [nm_cliente, cgc, email, telefone], (error, results) =>{
                 if(error){
                    reject(error);
                 }else{
