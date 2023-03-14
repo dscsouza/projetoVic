5ec454a38da4fea1b92b1dfb141544e274c1af3e
Victor Alexandre <alexanvictor@gmail.com>
2023-03-13 20:24:22 -0400

    Ajuste nas rotas

--- routes/cliente.js
+++ routes/cliente.js
@@ -2,8 +2,11 @@ const clienteController = require('../controllers/clienteController');
 const authenticateToken = require('../middlewares/authenticateToken');
 
 module.exports = app => {
+    app.get('/cadastraCliente', authenticateToken, (req, res) => {
+        res.render('inclui_cliente')
+      });
+
     app.post('/cliente', clienteController.cadastra);
-    app.post('/cadastraCliente', authenticateToken, clienteController.cadastra);
 }
 
 
--- routes/item.js
+++ routes/item.js
@@ -2,8 +2,9 @@ const itemController = require('../controllers/itemController');
 const authenticateToken = require('../middlewares/authenticateToken');
 
 module.exports = app => {
-    app.post('/item', itemController.cadastra);
-    app.get('/estoque', authenticateToken, (req, res) => {
-        res.render('inclui_item')
-      });
+  app.get('/estoque', authenticateToken, (req, res) => {
+    res.render('inclui_item')
+  });
+
+  app.post('/item', itemController.cadastra);
 }
