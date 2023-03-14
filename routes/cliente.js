61bf88e4c5971effe5c2425af26627f65b565b58
dscsouza <dscsouza@gmail.com>
2023-03-13 22:56:45 -0400

    incluida msg em branco a cada requisição get, para evitar erro

--- routes/cliente.js
+++ routes/cliente.js
@@ -3,7 +3,7 @@ const authenticateToken = require('../middlewares/authenticateToken');
 
 module.exports = app => {
     app.get('/cadastraCliente', authenticateToken, (req, res) => {
-        res.render('inclui_cliente')
+        res.render('inclui_cliente', { msg: '' })
       });
     app.get('/clientes', authenticateToken, clienteController.lista);
     app.post('/cliente', clienteController.cadastra);
--- routes/login.js
+++ routes/login.js
@@ -7,17 +7,17 @@ module.exports = app => {
     // Rota protegida por autenticação
     app.get('/protegido', authenticateToken, (req, res) => {
       console.log('autenticado...')  
-      res.render('dashboard', {user: req.user.username})
+      res.render('dashboard', { msg: 'Usuário logado com sucesso.' })
       });
     
     //para fins de testes essa rota encontra-se desprotegida
     //antes de ir para produção, reincluir o middleware authenticateToken
     app.get('/abreOs', authenticateToken, (req, res) => {
-      res.render('abertura_os')
+      res.render('abertura_os', { msg: '' })
     });
 
     app.get('/inclui_usuario', authenticateToken, (req, res) => {
-      res.render('inclui_usuario')
+      res.render('inclui_usuario', { msg: '' })
     });    
     
 
