8a0cc6eccd997a9711adc8d0dcb93b22122ade53
dscsouza <dscsouza@gmail.com>
2023-03-14 02:57:30 -0400

    implementado session ao invés do jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const custonExpress = require('./config/custon-express');
 const connection = require('./database/connection');
 
+
 require("dotenv").config();
 
 connection.connect((error) => {
@@ -9,6 +10,7 @@ connection.connect((error) => {
   }else{
     console.log('Conectado!');
       const app = custonExpress();
+
       app.listen(process.env.PORT, () => {
       console.log(`Servidor rodando na porta ${process.env.PORT}`);
     });
