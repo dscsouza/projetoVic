c353d17719c0ae25c95d637d36f0258fe0e61752
dscsouza <dscsouza@gmail.com>
2023-03-15 19:17:54 -0400

    atualização no layout

--- static/scripts/form_cadastra_cliente.js
+++ static/scripts/form_cadastra_cliente.js
@@ -35,20 +35,86 @@ function atualizarTabelaClientes() {
 atualizarTabelaClientes();
 
 
+function atualizaTabelaPesquisa() {
+  const tbody = document.getElementById('listaClientes');
+  tbody.innerHTML = '';
 
-nomeCliente = document.getElementById("searchClient")
-nomeCliente.addEventListener("keyup", searchcliente);
+  buscarClientes().then(clientes => {
+    clientes.forEach(cliente => {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td>${cliente.nm_cliente}</td>
+        <td>${cliente.CGC}</td>
+        <td>${cliente.email}</td>
+        <td>${cliente.telefone}</td>
+      `;
+      tbody.appendChild(tr);
+    });
+  });
+}
+
+
+
+searchCliente = document.getElementById("searchClient")
+searchCliente.addEventListener("keyup", searchcliente);
 
 function searchcliente(){
   
   criterioPesquisa = document.getElementById("searchItem")
+
+
+  criterio = criterioPesquisa.value
+  nomeCliente = searchCliente.value
   
-  // testando a captura dos valores
-  console.log(criterioPesquisa.value, nomeCliente.value)
-
-  //fetch
 
 
+  //fetch  -   /pesquisaAproximacao
+  // critério {atributo, valor}
 
 
+  const params = new URLSearchParams({
+    atributo: criterio,
+    valor: nomeCliente
+  });
+  
+  return fetch(`/pesquisaAproximacao?${params}`, {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => {
+    // retorna a resposta em formato json
+    return response.json();
+  })
+  .then(data =>{
+    return data;
+  })
+  .catch(error => {
+    // imprime no console o erro
+    console.error(error);
+  });
+
+  
+  
+}
+
+
+function atualizaTabelaAposPesquisa() {
+  const tbody = document.getElementById('listaClientes');
+  tbody.innerHTML = '';
+
+  
+  searchcliente().then(clientes => {
+    clientes.forEach(cliente => {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td>${cliente.nm_cliente}</td>
+        <td>${cliente.CGC}</td>
+        <td>${cliente.email}</td>
+        <td>${cliente.telefone}</td>
+      `;
+      tbody.appendChild(tr);
+    });
+  });
 }
