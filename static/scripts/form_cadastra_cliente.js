06331c58695b7fffc74a5f3809274bc426688fd4
DeyviMacOS <dscsouza@gmail.com>
2023-03-21 20:51:56 -0400

    menu exibido ao passar sobre cada linha da table

--- static/scripts/form_cadastra_cliente.js
+++ static/scripts/form_cadastra_cliente.js
@@ -1,8 +1,40 @@
 const form = document.getElementById('cadastroClientes');
 const divPagination = document.getElementById('divPagination')
 
+/////////////////////////////////////////////////////////////////////////
+// MENU EXIBIDO AO MOVER O MOUSE SOBRE CADA LINHA DA TABELA DE CLIENTES//
+/////////////////////////////////////////////////////////////////////////
+//exibe o menu
+function show_menu_row(e){
+  let x = e.querySelectorAll(".menuRow > .btn-group");
+  x[0].classList.remove("invisible")
+}
 
-// {nm_cliente, cgc, email, telefone}
+// oculta o menu assim que o mouse passa da linha
+function hide_menu_row(e){
+  let x = e.querySelectorAll(".menuRow > .btn-group");
+  x[0].classList.add("invisible")
+}
+
+//evento disparado ao clicar em excluir um registro
+function excluirRegistro(e){
+  //pega o cpf diretamente da tag data-cgc criada ao renderizar a tabela
+  cgc = e.getAttribute('data-cgc')
+  console.log('excluir registro de cpf:', cgc)
+  
+}
+//evento disparado ao clicar em editar um registro
+function editarRegistro(e){
+  //pega o cpf diretamente da tag data-cgc criada ao renderizar a tabela
+  cgc = e.getAttribute('data-cgc')
+  console.log('editar registro de cpf:', cgc)
+
+}
+
+
+
+
+
 
 // ENVIANDO OS DADOS PARA A ROTA /clientes
 
@@ -16,7 +48,6 @@ function pagination(totalPaginas, pgAtual){
   <ul class="pagination">`;
 
   for (let index = 1; index <= totalPaginas; index++) {
-    console.log(navPagination);
     ativa = index == pgAtual?'active':' ' // torna a página atual ativa, alterando seu estilo
     navPagination = navPagination + `<li class="page-item ${ativa}"><a class="page-link" style="cursor:pointer" onclick="atualizarTabelaClientes(${index})">${index}</a></li>`;
   }
@@ -75,10 +106,34 @@ function atualizarTabelaClientes(pgAtual) {
       const tr = document.createElement('tr');
       tr.innerHTML = `
         <td>${cliente.nm_cliente}</td>
-        <td>${cliente.CGC}</td>
+        <td class="CGC">${cliente.CGC}</td>
         <td>${cliente.email}</td>
         <td>${cliente.telefone}</td>
+        <td class="menuRow">
+        <div class="btn-group invisible" role="group" aria-label="Excluir ou Editar">
+          <button type="button" class="btn btn-outline-secondary" data-cgc="${cliente.CGC}" onclick="excluirRegistro(this)">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+            </svg>
+          </button>
+          <button type="button" class="btn btn-outline-secondary" data-cgc="${cliente.CGC}" onclick="editarRegistro(this)">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+              <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+            </svg>
+          </button>
+        </div>
+
+        
+        </td>
       `;
+      // Ao passar o mouse pelo item da lista(onmouseover), exibe a imagem de excluir
+      // ao retirar o mouse (onmouseout), oculta o a imagem de excluir
+      tr.setAttribute("onmouseover", "show_menu_row(this)");
+      tr.setAttribute("onmouseout", "hide_menu_row(this)");
+      
+      
+
+     
       tbody.appendChild(tr);
     });
   });
